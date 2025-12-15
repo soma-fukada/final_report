74189e6d807a509930055406439d88f156d21d6a
深田　壮真 <s256139q@st.go.tuat.ac.jp>
2025-12-15 22:54:33 +0900

    add type hints

--- source_code.py
+++ source_code.py
@@ -7,13 +7,13 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 import lightning as L
 
-# 1. モデル（ニューラルネットワーク）の定義
+# 1. モデルの定義
 class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
         return self.l1(x)
 
 class Decoder(nn.Module):
@@ -21,17 +21,17 @@ class Decoder(nn.Module):
         super().__init__()
         self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.l1(x)
 
-# 2. LightningModuleの定義（学習ループの中身）
+# 2. LightningModuleの定義
 class LitAutoEncoder(L.LightningModule):
     def __init__(self, encoder, decoder):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch: list[torch.Tensor], batch_idx: int) -> torch.Tensor:
         # training_step defines the train loop.
         x, y = batch
         x = x.view(x.size(0), -1)
